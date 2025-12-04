f10dcd61cf27f59fef5a5f635679de1697afb200
Oscar Yael Dinero Guerrero <2121100428@soy.utj.edu.mx>
2025-12-03 22:30:02 -0600

    apis

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+{
+    $this->routes(function () {
+        // Esto ya debería estar
+        Route::middleware('api')
+             ->prefix('api')
+             ->group(base_path('routes/api.php'));
+
+        Route::middleware('web')
+             ->group(base_path('routes/web.php'));
+    });
+}
+}
