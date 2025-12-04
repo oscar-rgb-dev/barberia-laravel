f630da668903050180faabdcaf30231cccb5ee77
Oscar Yael Dinero Guerrero <2121100428@soy.utj.edu.mx>
2025-12-04 01:28:46 -0600

    appservices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        
         // ✅ FORZAR CARGA DE RUTAS API AQUÍ
         Route::prefix('api')
             ->middleware('api')  // Middleware API, NO 'web'
